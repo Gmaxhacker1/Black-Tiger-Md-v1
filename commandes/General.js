ed7d1e823c0318eab38fadd27300f823005435f2
Gmaxhacker1 <151476499+Gmaxhacker1@users.noreply.github.com>
2024-11-18 12:58:24 +0300

    Update General.js

--- commandes/General.js
+++ commandes/General.js
@@ -55,9 +55,9 @@ zokou({ nomCom: "dev", categorie: "General", reaction: "🚘" }, async (dest, zk
     const { ms, mybotpic } = commandeOptions;
 
     const devs = [
-      { nom: "Anyway", numero: "255678892560" },
-      { nom: "᚛Ibrahim᚜", numero: "254710772666" },
-      { nom: "Baraka", numero: "255762190568" },
+      { nom: "Gmax", numero: "255622053093" },
+      { nom: "᚛Gmax᚜", numero: "255622053093" },
+      { nom: "Nyingi", numero: "255753853473" },
       // Ajoute d'autres développeurs ici avec leur nom et numéro
     ];
 
